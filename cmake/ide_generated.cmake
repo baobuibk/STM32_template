86f2b98db8bea3c5d7ac993ae566204a7b19e48c
baobuibk <buiqbao@gmail.com>
2025-11-05 13:18:41 +0700

    add CmakeList to all sub folders

--- cmake/ide_generated.cmake
+++ cmake/ide_generated.cmake
@@ -22,19 +22,43 @@ set(cpu_PARAMS ${cpu_PARAMS} ${CPU_FLAGS})
 
 # Linker script
 set(linker_script_SRC ${linker_script_SRC}
-	${CMAKE_CURRENT_SOURCE_DIR}/stm32f407xg_flash.ld
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/STM32F746NGHX_FLASH.ld
 )
 
 # Sources
 set(sources_SRCS ${sources_SRCS}
-	${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/test/Src/main.c
-	${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/test/Src/syscall.c
-	${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/test/Src/sysmem.c
-	${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/test/Src/startup_stm32f407xx.S
+    # Core sources
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Src/main.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Src/stm32f7xx_hal_msp.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Src/stm32f7xx_it.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Src/syscalls.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Src/sysmem.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Src/system_stm32f7xx.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Startup/startup_stm32f746nghx.s
+
+    # HAL driver sources (only the ones present in tree)
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_cortex.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_exti.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash_ex.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c_ex.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr_ex.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc_ex.c
 )
 
 # Include directories
 set(include_c_DIRS ${include_c_DIRS}
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Core/Inc
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/STM32F7xx_HAL_Driver/Inc
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/CMSIS/Device/ST/STM32F7xx/Include
+    ${CMAKE_CURRENT_SOURCE_DIR}/ide_gen/Drivers/CMSIS/Include
 )
 set(include_cxx_DIRS ${include_cxx_DIRS}
 )
@@ -43,7 +67,8 @@ set(include_asm_DIRS ${include_asm_DIRS}
 
 # Symbols definition
 set(symbols_c_SYMB ${symbols_c_SYMB}
-	RTE_DEVICE_STARTUP_STM32F4XX
+    USE_HAL_DRIVER
+    STM32F746xx
 )
 set(symbols_cxx_SYMB ${symbols_cxx_SYMB}
 )
